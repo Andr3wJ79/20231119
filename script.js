e21da29214066d336c100bbc4359897f79afe07f
andr3wj <andr3wj.1979@outlook.com>
2023-11-19 13:45:39 +0100

    két funkció hozzáadása

--- script.js
+++ script.js
@@ -16,3 +16,28 @@ function korKT(sugar) {
     return eredmeny;
 }
 console.log(korKT(12));
+function legkisebbHengerurtartalom(autok) {
+    var eredmeny = 0;
+    var aktualis = autok[0].hengerurtartalom;
+    for (var i = 1; i < autok.length; i++) {
+        if (aktualis > autok[i].hengerurtartalom) {
+            eredmeny = i;
+        }
+    }
+    return eredmeny;
+}
+var a1 = { gyarto: "Opel", tipus: "Astra", hengerurtartalom: 1400, benzinesE: true };
+var a2 = { gyarto: "Opel", tipus: "Combo", hengerurtartalom: 1399, benzinesE: false };
+var a3 = { gyarto: "Nissan", tipus: "350Z", hengerurtartalom: 3500, benzinesE: true };
+var autok = [a1, a2, a3];
+console.log(legkisebbHengerurtartalom(autok));
+function benzinesEDarab(autok) {
+    var eredmeny = 0;
+    for (var i = 0; i < autok.length; i++) {
+        if (autok[i].benzinesE) {
+            eredmeny += 1;
+        }
+    }
+    return eredmeny;
+}
+console.log(benzinesEDarab(autok));
--- script.ts
+++ script.ts
@@ -21,11 +21,41 @@ function korKT(sugar:number):[number,number]{
 
 console.log(korKT(12));
 
-interface Auto{
+interface auto{
     gyarto:string;
-    tipus:number;
+    tipus:string;
     hengerurtartalom: number;
     benzinesE : boolean;
 }
 
+function legkisebbHengerurtartalom(autok:auto[]):number{
+    var eredmeny:number = 0;
+    var aktualis:number = autok[0].hengerurtartalom;
 
+    for(var i:number = 1; i < autok.length; i++){
+        if (aktualis > autok[i].hengerurtartalom){
+            eredmeny = i;
+        }
+    }
+    return eredmeny;
+}
+
+var a1:auto = {gyarto:"Opel", tipus:"Astra", hengerurtartalom:1400, benzinesE:true};
+var a2:auto = {gyarto:"Opel", tipus:"Combo", hengerurtartalom:1399, benzinesE:false};
+var a3:auto = {gyarto:"Nissan", tipus:"350Z", hengerurtartalom:3500, benzinesE:true};
+var autok:auto[] = [a1,a2,a3]
+console.log(legkisebbHengerurtartalom(autok));
+
+function benzinesEDarab(autok:auto[]):number{
+    var eredmeny:number = 0;
+
+    for(var i:number = 0; i < autok.length; i++){
+        if (autok[i].benzinesE){
+            eredmeny += 1;
+        }
+    }
+    return eredmeny;
+
+}
+
+console.log(benzinesEDarab(autok));
